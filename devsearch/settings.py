989e9eff75f2c180e11488c3965f23d351c793c0
Tanuj <tanujrm44@gmail.com>
2023-10-07 14:57:56 +0530

    set for deployment

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2hxfne0co_)it*#bk5@_xl14lllq+8wf-$6a4ak^k3-1!2lwon'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 
